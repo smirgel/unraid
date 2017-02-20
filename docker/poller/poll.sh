2aba7f4671e4227e1f11f73b065ea021cc47f028
maglun2 <magnus.lundin@bisnode.com>
2017-02-20 12:56:52 +0100

    tweaks

--- docker/poller/poll.sh
+++ docker/poller/poll.sh
@@ -2,11 +2,23 @@
 
 #curl -v https://httpbin.org/get
 
+for var in FTP_HOST FTP_USER FTP_PASSWORD; do
+  if [ -z "${!var}" ] ; then
+    echo "$var must be set"
+    exit 1
+  fi
+done
+
 prev_ext_ip=""
 while true; do
   ext_ip=$(curl -fsS https://httpbin.org/get | jq -r .origin)
   if [[ $ext_ip != $prev_ext_ip ]]; then
-    echo "$ext_ip"
+    echo "IP changed: $ext_ip"
+    echo "Create html file"
+    env EXTERNAL_IP=$ext_ip envsubst < testing.html.in > testing.html
+    echo "Upload html file"
+    ncftpput -v -u $FTP_USER -p $FTP_PASSWORD $FTP_HOST public_html/ testing.html
+    echo "done"
   fi
   sleep 60
 done
