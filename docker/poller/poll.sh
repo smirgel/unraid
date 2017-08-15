6cd077a1d138235846458f624cd346d668a49987
maglun2 <magnus.lundin@bisnode.com>
2017-08-15 13:05:49 +0200

    tweaks

--- docker/poller/poll.sh
+++ docker/poller/poll.sh
@@ -18,27 +18,27 @@ while true; do
   ext_ip=$(curl -fsS https://httpbin.org/get | jq -re .origin)
   RC=$?
   if [[ $RC -eq 0 ]]; then
-    if [[ $ext_ip != "$prev_ext_ip" ]]; then
-      echo "IP changed: $ext_ip"
+    if [[ ${ext_ip} != "${prev_ext_ip}" ]]; then
+      echo "IP changed: ${ext_ip}"
 
       echo ''
       echo 'Getting ips.txt'
-      ncftpget -V -u "$FTP_USER" -p "$FTP_PASSWORD" "$FTP_HOST" . public_html/ips.txt
-      echo "$(date): $ext_ip" >> ips.txt
+      ncftpget -V -u "${FTP_USER}" -p "${FTP_PASSWORD}" "${FTP_HOST}" . public_html/ips.txt
+      echo "$(date): ${ext_ip}" >> ips.txt
       tail -5 ips.txt
 
       echo ''
       echo 'Create html file(s)'
-      env EXTERNAL_IP="$ext_ip" envsubst < testing.html.in > testing.html
-      env EXTERNAL_IP="$ext_ip" envsubst < ip.html.in > ip.html
+      env EXTERNAL_IP="${ext_ip}" envsubst < testing.html.in > testing.html
+      env EXTERNAL_IP="${ext_ip}" envsubst < ip.html.in > ip.html
 
       echo ''
       echo 'Upload files'
-      ncftpput -V -u "$FTP_USER" -p "$FTP_PASSWORD" "$FTP_HOST" public_html/ testing.html ip.html ips.txt
+      ncftpput -V -u "${FTP_USER}" -p "${FTP_PASSWORD}" "${FTP_HOST}" public_html/ testing.html ip.html ips.txt
 
       echo ''
       echo 'Done'
-      prev_ext_ip=$ext_ip
+      prev_ext_ip=${ext_ip}
     fi
   else
     echo "$(date): curl failed"
